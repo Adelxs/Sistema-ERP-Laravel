dce7f3cf4d5c60886d44410854d3d0b865be9a3a
Adelxs <adelbros1015@gmail.com>
2026-06-10 13:17:03 -0400

    agregando routes

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,10 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UsersController;
 use App\Http\Controllers\PurchaseController;
+use App\Http\Controllers\InventoryController;
+use App\Http\Controllers\SaleController;
+use App\Http\Controllers\SupplierController;
+use App\Http\Controllers\CustomerController;
 
 
 
@@ -16,3 +20,7 @@ Route::resource('categories', CategoryController::class);
 Route::resource('products', ProductController::class);
 Route::resource('users', UsersController::class);
 Route::resource('purchase', PurchaseController::class);
+Route::resource('sales', SaleController::class);
+Route::resource('inventory', InventoryController::class);
+Route::resource('suppliers', SupplierController::class);
+Route::resource('customers', CustomerController::class);
